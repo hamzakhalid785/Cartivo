3d5adb2a2c6a0d49b77db22db309c95b968b492b
hamzakhalid785 <hamzakdev1@gmail.com>
2026-08-08 19:50:22 +0500

    Initial Cartivo setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+return (
+  <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 className="text-5x1 font-bold text-gray-900">
+      Cartivo
+    </h1>
+  </div>
+)
+}
+
+export default App
