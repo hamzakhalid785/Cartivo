bba7bea9abad99850b58170aea88da12fc144b1c
hamzakhalid785 <hamzakdev1@gmail.com>
2026-08-13 11:50:52 +0500

    Fix production settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,12 +30,6 @@ DEBUG = os.getenv('DEBUG') == 'True'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7f*!&u8a2lr4964r(^*2h6&-uhz(o*6b8^+t67$$0u*2sye=(c'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
